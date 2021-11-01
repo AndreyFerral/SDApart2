69d50d8809dd7d47bf25d4ee9afaac0ada047c2d
AndreyPC <tigra9900@mail.ru>
2021-11-01 18:16:24 +1000

    lab2 гамильтонов цикл готов, шахматы в процессе

---
+++ lab2_task2/lab2_task2.cpp
@@ -0,0 +1,69 @@
+﻿/*
+19.	В центре шахматной доски стоит конь. 
+Найти все расположения восьми ладей, не угрожающих друг другу, 
+при которых конь не угрожает ни одной из ладей.
+*/
+
+#include <iostream>
+using namespace std;
+
+void setRook(int& myarr, int sizeMatrix, int i)
+{
+	for (int j = 0; j < sizeMatrix; ++j)
+	{
+		if (myarr[i][j] != 1) {
+			myarr[i][j] = 1;
+			setRook(myarr, sizeMatrix, i);
+			cout << i << " " << j << " ";
+		}
+	}
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	const int sizeMatrix = 8;
+
+	int chessBoard[sizeMatrix][sizeMatrix] =
+	{
+		0,0,0,0,0,0,0,0,
+		0,0,0,1,0,1,0,0,
+		0,0,1,0,0,0,1,0,
+		0,0,0,0,1,0,0,0,
+		0,0,1,0,0,0,1,0,
+		0,0,0,1,0,1,0,0,
+		0,0,0,0,0,0,0,0,
+		0,0,0,0,0,0,0,0
+	};
+
+	// Выводим на экран исходный граф
+	cout << "Шахматная доска с конём: " << "\n";
+
+	for (int i = 0; i < 2; ++i)
+	{
+		cout << "    ";
+		for (int j = 0; j < sizeMatrix; ++j)
+		{
+			if (i == 0) cout << j + 1 << " ";
+			if (i == 1) cout << "--";
+		}
+		cout << "\n";
+	}
+
+	for (int i = 0; i < sizeMatrix; ++i)
+	{
+		cout << i + 1 << " | ";
+		for (int j = 0; j < sizeMatrix; ++j)
+		{
+			cout << chessBoard[i][j] << " ";
+		}
+		cout << "\n";
+	}
+	cout << "\n";
+
+	for (int i = 0; i < sizeMatrix; ++i)
+	{
+		setRook(chessBoard, sizeMatrix, i);
+	}
+}
--- lab2_test/lab2_test.cpp
+++ lab2_test/lab2_test.cpp
@@ -1,175 +1,85 @@
-﻿/*
-9.	Кликой называется множество вершин графа, любые две из которых смежны.
-	Найти клику, имеющую максимальное количество вершин.
-*/
-
-#include <iostream>
-#include <vector>
+﻿#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#define N 10
 using namespace std;
 
-// Функция, которая выводит вектор векторов
-void outputVectorOfVectors(vector<vector<int>> vector)
-{
-	for (int i = 0; i < vector.size(); ++i)
-	{
-		cout << i << " -> ";
-		for (int j = 0; j < vector[i].size(); ++j)
-		{
-			cout << vector[i][j] << " ";
-		}
-		cout << "\n";
-	}
+void displaytheSolution(int path[]);
+
+bool isSafe(int n, bool g[N][N], int path[], int pos) {
+    if (g[path[pos - 1]][n] == 0)
+        return false;
+    for (int i = 0; i < pos; i++)
+        if (path[i] == n)
+            return false;
+    return true;
 }
-
-// Функция, которая выводит первую максимальную клику
-void outputMaxClique(vector<vector<int>> vector) 
-{
-	int maxCliqueSize = 0;
-	int maxCliqueIndex = 0;
-
-	// Найдем первую максимальную клику
-	for (int i = 0; i < vector.size(); ++i)
-	{
-		if (vector[i].size() > maxCliqueSize)
-		{
-			maxCliqueSize = vector[i].size();
-			maxCliqueIndex = i;
-		}
-	}
-
-	// Выведем первую максимальную клику
-	cout << "Первая максимальная клика: " << "\n";
-	for (int i = 0; i < vector[maxCliqueIndex].size(); ++i)
-	{
-		cout << vector[maxCliqueIndex][i] << " ";
-	}
-	cout << "\n";
+bool hamiltonianCycle(bool g[N][N], int path[], int pos) {
+    //If all vertices are included in Hamiltonian Cycle
+    if (pos == N) {
+        if (g[path[pos - 1]][path[0]] == 1)
+            return true;
+        else
+            return false;
+    }
+    for (int n = 1; n < N; n++) {
+        if (isSafe(n, g, path, pos)) { //Check if this vertex can be added to Hamiltonian Cycle
+            path[pos] = n;
+            //recur to construct rest of the path
+            if (hamiltonianCycle(g, path, pos + 1) == true)
+                return true;
+            path[pos] = -1; //remove vertex if it doesn’t lead to the solution
+        }
+    }
+    return false;
 }
-
-// Функция, проверяющая две вершины - являеются ли они кликами
-bool isClique(vector<vector<int>> graph, int vertex1, int vertex2)
-{
-	for (int i = 0; i < graph[vertex1].size(); ++i)
-	{
-		if (graph[vertex1][i] == vertex2) return true;
-	}
-	return false;
+bool hamCycle(bool g[N][N]) {
+    int* path = new int[N];
+    for (int i = 0; i < N; i++)
+        path[i] = -1;
+    //put vertex 0 as the first vertex in the path. If there is a Hamiltonian Cycle, then the path can be started from any point
+    //of the cycle as the graph is undirected
+    path[0] = 0;
+    if (hamiltonianCycle(g, path, 1) == false) {
+        cout << "\nCycle does not exist" << endl;
+        return false;
+    }
+    displaytheSolution(path);
+    return true;
 }
+void displaytheSolution(int p[]) {
+    cout << "Cycle Exists:";
+    cout << " Following is one Hamiltonian Cycle \n" << endl;
+    for (int i = 0; i < N; i++)
+        cout << p[i] << " ";
+    cout << p[0] << endl;
+}
+int main() {
+    /*
+    bool g[N][N] = 
+    {  
+       {0, 1, 0, 1, 1},
+       {0, 0, 1, 1, 0},
+       {0, 1, 0, 1, 1},
+       {1, 1, 1, 0, 1},
+       {0, 1, 1, 0, 0},
+    };
+    */
 
-int main()
-{
-	setlocale(LC_ALL, "Russian");
+    bool matrixAdjacency[N][N] =
+    {
+        0,0,0,0,0,1,0,0,0,0,
+        0,0,1,0,0,0,1,0,0,0,
+        0,1,0,1,0,0,0,1,0,0,
+        0,0,1,0,1,0,0,0,1,0,
+        1,0,0,1,0,0,0,0,0,1,
+        0,0,0,0,0,0,1,0,0,1,
+        0,0,0,1,0,0,0,1,0,0,
+        0,0,0,0,1,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,0,1,
+        0,0,0,0,0,0,0,0,0,0
+    };
 
-	const int sizeArray = 10;
-
-	// Матрица смежности
-	int matrixAdjacency[sizeArray][sizeArray] =
-	{
-		0,0,0,0,0,1,0,0,0,0,
-		0,0,1,0,0,0,1,0,0,0,
-		0,1,0,1,0,0,0,1,0,0,
-		0,0,1,0,1,0,0,0,1,0,
-		1,0,0,1,0,0,0,0,0,1,
-		0,0,0,0,0,0,1,0,0,1,
-		0,0,1,1,0,0,0,1,0,0,
-		0,0,0,0,1,0,0,0,0,0,
-		0,0,0,0,0,0,0,0,0,1,
-		0,0,0,0,0,0,0,0,0,0
-	};
-
-	// Выводим на экран исходный граф
-	cout << "Исходный граф: " << "\n";
-	for (int i = 0; i < sizeArray; ++i)
-	{
-		for (int j = 0; j < sizeArray; ++j)
-		{
-			cout << matrixAdjacency[i][j] << " ";
-		}
-		cout << "\n";
-	}
-	cout << "\n";
-
-	vector<vector<int>> adjacentVertexs;
-	vector<int> helpVertexs;
-
-	// Заполняем вектор вектором, в котором вершина со смежными вершинами
-	for (int i = 0; i < sizeArray; ++i)
-	{
-		for (int j = 0; j < sizeArray; ++j)
-		{
-			// Если значение 1, то добавляет в вспомогателей вектор
-			// Последнее условие - не учитывать значения на главной диагонали
-			if ((matrixAdjacency[i][j] == 1 || matrixAdjacency[j][i] == 1) && i != j)
-			{
-				helpVertexs.push_back(j);
-			}
-		}
-		adjacentVertexs.push_back(helpVertexs);
-		helpVertexs.clear();
-	}
-
-	// Выводим на экран вектор векторов, в котором вершина со смежными вершинами
-	cout << "Вершины со смежными вершинами: " << "\n";
-	outputVectorOfVectors(adjacentVertexs);
-
-	vector<vector<int>> clique; // вектор клик
-	vector<int> helpClique; // клика
-	 
-	bool checkClique = true;
-
-	// Заполняем вектор вектором кликами
-	for (int i = 0; i < adjacentVertexs.size(); ++i)
-	{
-		// cout << "Добавил " << i << " \n";
-		helpClique.push_back(i); // добавим первое значение в клику
-
-		for (int j = 0; j < adjacentVertexs[i].size(); ++j)
-		{	
-			for (int it = 0; it < helpClique.size(); ++it)
-			{
-				// cout << "Сравнение " << adjacentVertexs[i][j] << " и " << help[it] << " \n";
-
-				// Если эта вершина не является смежной со всеми вершинами в клике, то не будем добавлять её в клику
-				if (isClique(adjacentVertexs, adjacentVertexs[i][j], helpClique[it]) == false)
-				{
-					checkClique = false;
-				}
-
-			}
-			
-			// Если вершина является смежной со всеми вершинами в клике, то добавим её к клике
-			if (checkClique == true) 
-			{
-				// cout << "Добавил " << adjacentVertexs[i][j] << " \n";
-				helpClique.push_back(adjacentVertexs[i][j]);
-			}
-			else { checkClique = true; }
-			
-		}
-
-		// Добавим клику в вектор клик
-		clique.push_back(helpClique);
-
-		// Очистим вектор клик для следующей итерации
-		helpClique.clear();
-	}
-	cout << "\n";
-
-	/*
-	// Выводим на экран вектор векторов, в котором вершина со своей кликой
-	cout << "Вершины с кликами: " << "\n";
-	for (int i = 0; i < clique.size(); ++i)
-	{
-		cout << i << " -> ";
-		for (int j = 1; j < clique[i].size(); ++j)
-		{
-			cout << clique[i][j] << " ";
-		}
-		cout << "\n";
-	}
-	cout << "\n";
-	*/
-
-	// Выводим на экран первую максимальную клику
-	outputMaxClique(clique);
+    hamCycle(matrixAdjacency);
+    return 0;
 }
